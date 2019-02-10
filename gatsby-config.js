4101f01202df0dbd287204990d2159d740cc72d1
Gonzalo Rocha <grochadc@gmail.com>
2019-02-10 17:27:39 -0600

    Add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    title: `Gonzalo Rocha Web Portfolio`,
+    description: `My online filmaking and design portfolio`,
+    author: `@medicengonzo`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
